8e64f9a16bdc8ddf20d5ef29d99646a95e1db96e
Rajesh Senapati <rksenapati.9@gmail.com>
2025-06-15 12:16:19 +0530

    task 1

---
+++ task1.py
@@ -0,0 +1,10 @@
+firstNumber = int(input("Enter first number: "))
+secondNumber = int(input("Enter second number: "))
+sum = firstNumber + secondNumber
+print("Addition", sum)
+subtraction = firstNumber - secondNumber
+print("Subtraction", subtraction)
+multiplication = firstNumber * secondNumber
+print("Multiplication",multiplication)
+division = firstNumber / secondNumber
+print("Division",division)
